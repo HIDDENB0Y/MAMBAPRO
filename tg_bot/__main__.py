557c5abd492f6b270c2d5ef8edc8945135290182
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-26 20:45:22 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,7 +27,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hᴇʟʟᴏ Tʜᴇʀᴇ, I ᴀᴍ  [𝗞𝗛𝗨𝗦𝗛𝗜](t.me/deadly_fighters)\n                 
-I ᴀᴍ Aɴ Aɴɪᴍᴇ Tʜᴇᴍᴇᴅ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ Aɴᴅ I ᴡɪʟʟ Hᴇʟᴘ U Iɴ Mᴀɴᴀɢɪɴɢ Uʀ Gʀᴏᴜᴘ\n
+I ᴀᴍ Aɴ Aɴɪᴍᴇ Tʜᴇᴍᴇᴅ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ Aɴᴅ I ᴡɪʟʟ Hᴇʟᴘ U Iɴ Mᴀɴᴀɢɪɴɢ Uʀ Gʀᴏᴜᴘ
           
 ✪ Hɪᴛ /help Tᴏ Kɴᴏᴡ Aʙᴏᴜᴛ Mʏ Mᴏᴅᴜʟᴇs ✪ \n
 
@@ -141,13 +141,13 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_photo(
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="💝 Hᴇʟᴘ 💝",
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="💖 Hᴇʟᴘ 💖",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="💖 Sᴜᴘᴘᴏʀᴛ Gʀᴘ 💖",
                                                                        url="t.me/DEADLY_USERBOT")],
                                                                                    [InlineKeyboardButton(text="✔️ Aᴅᴅ Mᴇ Iɴ Uʀ ɢʀᴘ",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="❤️ Oᴡɴᴇʀ ❤️",
+                                                                                   InlineKeyboardButton(text="💖 Oᴡɴᴇʀ 💖",
                                                                        url="t.me/DEADLY_OWNER_SAMEER")
                                                                                  ]]))
 
